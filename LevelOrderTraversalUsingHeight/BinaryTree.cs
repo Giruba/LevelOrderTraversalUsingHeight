39aeafba748bcd1700d4eea50e51542e161b267b
Giruba Beulah SE <gibeulah@microsoft.com>
2019-03-14 00:34:54 +0530

    Add project files.

---
+++ LevelOrderTraversalUsingHeight/BinaryTree.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelOrderTraversalUsingHeight
+{
+    class BinaryTree
+    {
+        BinaryTreeNode root;
+
+        public BinaryTree() { }
+
+        public BinaryTree(BinaryTreeNode binaryTreeNode) {
+            root = binaryTreeNode;
+        }
+
+        public BinaryTreeNode GetRoot() {
+            return root;
+        }
+
+        public void SetRoot(BinaryTreeNode binaryTreeNode) {
+            root = binaryTreeNode;
+        }
+
+        public BinaryTreeNode Insert(BinaryTreeNode root, int data) {
+            if (root == null) {
+                root = new BinaryTreeNode(data);
+                return root;
+            }
+            if (data < root.GetData())
+            {
+                root.SetLeft(Insert(root.GetLeft(), data));
+            }
+            else if(data > root.GetData()) {
+                root.SetRight(Insert(root.GetRight(), data));
+            }
+
+            return root;
+        }
+
+        public void PrintInorderTraversal(BinaryTreeNode binaryTreeNode) {
+            if (binaryTreeNode == null) {
+                return;
+            }
+
+            PrintInorderTraversal(binaryTreeNode.GetLeft());
+            Console.Write(binaryTreeNode.GetData() + " ");
+            PrintInorderTraversal(binaryTreeNode.GetRight());
+        }
+
+        public int GetHeight(BinaryTreeNode binaryTreeNode) {
+            if (binaryTreeNode == null) {
+                return 0;
+            }
+
+            return Math.Max(GetHeight(binaryTreeNode.GetLeft()),
+                GetHeight(binaryTreeNode.GetRight())) + 1 ;
+        }
+
+        public void LeftOrderTraversal(BinaryTreeNode binaryTreeNode) {
+            int height = GetHeight(binaryTreeNode);
+
+            for (int level = 0; level < height; level++) {
+                _PrintLeftOrderTraversal(binaryTreeNode, level);
+            }
+        }
+
+        public void _PrintLeftOrderTraversal(BinaryTreeNode binaryTreeNode, int level) {
+            if (binaryTreeNode == null) {
+                return;
+            }
+
+            if (level == 0) {
+                Console.Write(binaryTreeNode.GetData()+" ");
+            }
+            _PrintLeftOrderTraversal(binaryTreeNode.GetLeft(), level - 1);
+            _PrintLeftOrderTraversal(binaryTreeNode.GetRight(), level-1);
+        }
+    }
+}
---
+++ LevelOrderTraversalUsingHeight/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LevelOrderTraversalUsingHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Level order Traversal using Height of tree!");
+            Console.WriteLine("-------------------------------------------");
+
+            BinaryTree tree = GetTreeFromInput();
+            Console.WriteLine("Printing Inorder traversal------------");
+            tree.PrintInorderTraversal(tree.GetRoot());
+            Console.WriteLine();
+            Console.WriteLine("Printing Level order traversal--------");
+            tree.LeftOrderTraversal(tree.GetRoot());
+            Console.ReadLine();
+        }
+
+        private static BinaryTree GetTreeFromInput() {
+            BinaryTree binaryTree = null;
+
+            Console.WriteLine("Enter the number of nodes in the Binary tree");
+            try
+            {
+                int numberNodes = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the values of the nodes" +
+                    "separated by space");
+                binaryTree = new BinaryTree();
+                String[] elements = Console.ReadLine().Split(' ');
+                for (int i = 0; i < numberNodes; i++)
+                {
+                    binaryTree.SetRoot(binaryTree.Insert(binaryTree.GetRoot(), 
+                        int.Parse(elements[i])));
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+
+            return binaryTree;
+        }
+    }
+}
